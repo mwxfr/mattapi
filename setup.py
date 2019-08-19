f7a6e7a4038fd78530e95c581f0834fab1ad744f
Matt Wobensmith <mwobensmith@mozilla.com>
2019-08-19 16:31:22 -0700

    Moving files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import platform
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = 'mattapi'
-PACKAGE_VERSION = '0.3'
+PACKAGE_VERSION = '0.4'
 
 INSTALL_REQUIRES = [
 'coloredlogs==10.0',
